f127b57d2230a9ebddb4a58ad31f664c88cc18b6
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-12-18 18:49:42 -0300

    feat: virtualization with react virtualized

--- src/pages/components/SearchResults.tsx
+++ src/pages/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from 'react-virtualized'
+
 import { ProductItem } from './ProductItem'
 
 interface SearchResultsProps {
@@ -16,17 +18,24 @@ export function SearchResults({
   totalPrice,
   onAddToWishlist
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => (
+    <div key={key} style={style}>
+      <ProductItem product={results[index]} onAddToWishlist={onAddToWishlist} />
+    </div>
+  )
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => (
-        <ProductItem
-          key={product.id}
-          product={product}
-          onAddToWishlist={onAddToWishlist}
-        />
-      ))}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
